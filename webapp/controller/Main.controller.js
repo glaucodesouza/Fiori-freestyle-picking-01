5fcb2732d190f8a03ea36d0ab6ac42503d4a640e
glauco.r3 <glauco.r3@gmail.com>
2024-08-11 19:24:17 -0300

    view 1, navigation

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -207,16 +207,39 @@ sap.ui.define([
             },
             onLinePress: function (oEvent) {
     
-                let oItem, oCtx;
-                oItem = oEvent.getSource();
-                oCtx = oItem.getBindingContext();
                 let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 
-                //aqui é quando já há lotes no picking
-                oRouter.navTo("RouteView2",{
-                    Nuofe : oCtx.getProperty("Transporte"),
-                    Ticket : oCtx.getProperty("Status")
-                });
+                try {
+                    let oItem, oCtx;
+                    oItem = oEvent.getSource();
+                    oCtx = oItem.getBindingContext();
+                    
+
+                    //aqui é quando já há lotes no picking
+                    oRouter.navTo("RouteMainDetail",{
+                        Transporte : oCtx.getProperty("Transporte"),
+                        Status : oCtx.getProperty("Status") ? oCtx.getProperty("Status") : '1'
+                    });
+                
+                } catch (error) {
+                    //se nao conseguir pegar o transporte no oEvent,
+                    //procurar na linha que o usuário clicou
+                    let aIndexClicado = oEvent.mParameters.id.match(/\d+$/g);
+                    let nIndexClicado = aIndexClicado[0];
+    
+                    let loopIndex = 0;
+                    this.getView().byId("idTable").getItems().forEach(function(element) {
+                        if (parseInt(loopIndex) == parseInt(nIndexClicado)) {
+                            let aTransporte = element.getBindingContext().sPath.match(/'\d+'/g);
+                            let sTransporte = aTransporte[0];
+                            oRouter.navTo("RouteMainDetail",{
+                                Transporte : sTransporte,
+                                Status : '1'
+                            });
+                        }
+                        loopIndex++;
+                    });
+                }
             }
         });
     });
